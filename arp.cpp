e4e359a231b8c7a54d95aadf7189b858efd42973
adrian <adrian_lukaszyk@wp.pl>
2023-05-15 21:42:23 +0200

    parsing input and ip validation added

--- arp.cpp
+++ arp.cpp
@@ -1,17 +1,37 @@
 #include "inc/ether.hpp"
 #include "inc/arp.hpp"
 
+#include <cstdlib>
 #include <vector>
 #include <string_view>
+
+#include <arpa/inet.h>
 
 namespace input_args {
     void parse_args(int argc, char *argv[]);
 
+    bool valid_ip(const std::string &ip);
+
 }; //input_args
 
-void input_args::parse_args(int argc, char *argv[]) {
+void input_args::parse_args([[maybe_unused]]int argc, [[maybe_unused]] char *argv[]) {
+    if(argc != 2) {
+        std::string err_str = "Nie podano adresu IP. Uzycie programu:\n";
+        err_str += "<nazwa-programu> <docelowy-adres-IP>, przykl: <<  ./arp 192.168.0.2  >>\n";
+        std::cout << err_str;
+        std::exit(EXIT_FAILURE);
+    }
+        
     const std::vector<std::string_view> args(argv + 1, argv + argc);
+    for(const auto &arg : args) {
+        
+    }
 };
+
+bool input_args(const std::string &ip) {
+    struct sockaddr_in6 sa;
+    return inet_pton(AF_INET, ip.c_str(), &(sa.sin6_addr)) != 0;
+}
 
 int main([[maybe_unused]]int argc, [[maybe_unused]] char *argv[]) {
     input_args::parse_args(argc, argv);

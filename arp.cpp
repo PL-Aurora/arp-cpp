47e51e083b461f7647733c6b05341ac85c61d458
adrian <adrian_lukaszyk@wp.pl>
2023-05-15 22:38:58 +0200

    refactor

--- arp.cpp
+++ arp.cpp
@@ -9,7 +9,6 @@
 
 namespace input_args {
     void parse_args(int argc, char *argv[]);
-
     bool valid_ip(const std::string &ip);
 
 }; //input_args
@@ -17,18 +16,23 @@ namespace input_args {
 void input_args::parse_args([[maybe_unused]]int argc, [[maybe_unused]] char *argv[]) {
     if(argc != 2) {
         std::string err_str = "Nie podano adresu IP. Uzycie programu:\n";
-        err_str += "<nazwa-programu> <docelowy-adres-IP>, przykl: <<  ./arp 192.168.0.2  >>\n";
+        err_str += "<nazwa-programu> <docelowy-adres-IP>, przykl: <<  " + 
+                static_cast<std::string>(argv[0]) 
+                + " 192.168.0.2  >>\n";
         std::cout << err_str;
         std::exit(EXIT_FAILURE);
     }
         
-    const std::vector<std::string_view> args(argv + 1, argv + argc);
+    const std::vector<std::string> args(argv + 1, argv + argc);
     for(const auto &arg : args) {
-        
+        if(input_args::valid_ip(arg)){
+            std::cout << "valid IP\n";
+            // args.push_back(arg);
+        }
     }
 };
 
-bool input_args(const std::string &ip) {
+bool input_args::valid_ip(const std::string &ip) {
     struct sockaddr_in6 sa;
     return inet_pton(AF_INET, ip.c_str(), &(sa.sin6_addr)) != 0;
 }

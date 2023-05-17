0493b411bd7731d3ab57bf2b3d5e4d10a7079399
adrian <adrian_lukaszyk@wp.pl>
2023-05-17 21:21:33 +0200

    rewrite ip validation from command line

--- arp.cpp
+++ arp.cpp
@@ -7,13 +7,16 @@
 
 #include <arpa/inet.h>
 
+static std::array<uint8_t, IPALEN> ip_dst;
+
 namespace input_args {
     void parse_args(int argc, char *argv[]);
-    bool valid_ip(const std::string &ip);
+    // bool valid_ip(const std::string &ip);
+    bool valid_ip(char *ip);
 
 }; //input_args
 
-void input_args::parse_args([[maybe_unused]]int argc, [[maybe_unused]] char *argv[]) {
+void input_args::parse_args(int argc, char *argv[]) {
     if(argc != 2) {
         std::string err_str = "Nie podano adresu IP. Uzycie programu:\n";
         err_str += "<nazwa-programu> <docelowy-adres-IP>, przykl: <<  " + 
@@ -22,22 +25,27 @@ void input_args::parse_args([[maybe_unused]]int argc, [[maybe_unused]] char *arg
         std::cout << err_str;
         std::exit(EXIT_FAILURE);
     }
-        
-    const std::vector<std::string> args(argv + 1, argv + argc);
-    for(const auto &arg : args) {
-        if(input_args::valid_ip(arg)){
-            std::cout << "valid IP\n";
-            // args.push_back(arg);
-        }
+
+    if(!input_args::valid_ip(argv[1])) {
+        std::cerr << "Invalid IP address, exiting..." << std::endl;
+        std::exit(EXIT_FAILURE);
     }
 };
+bool input_args::valid_ip(char *ip) {
+    int cnt = 0;
+    struct in_addr addr;
+    if(inet_aton(ip, &addr) == 0) return false;
 
-bool input_args::valid_ip(const std::string &ip) {
-    struct sockaddr_in6 sa;
-    return inet_pton(AF_INET, ip.c_str(), &(sa.sin6_addr)) != 0;
+    for(auto it = ip_dst.begin(); it != ip_dst.end(); ++it, ++cnt)
+        *it = static_cast<uint8_t>((addr.s_addr >> (8 * cnt)) & 0xff);
+
+    /* for(auto &i : ip_dst) {
+        std::cout << static_cast<int>(i) << std::endl;
+    } */
+    return true;
 }
 
-int main([[maybe_unused]]int argc, [[maybe_unused]] char *argv[]) {
+int main(int argc, char *argv[]) {
     input_args::parse_args(argc, argv);
 
     init_device();

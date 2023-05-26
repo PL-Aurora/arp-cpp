96cc3e4e466ba0818fda901dcc4d6f37b4ba5d69
Adrian <adr.luk.git@wp.pl>
2023-05-26 09:54:40 +0200

    added creation receiving socket function

--- inc/receive_socket.h
+++ inc/receive_socket.h
@@ -4,19 +4,21 @@ extern "C" {
 
 #include <string.h>
 #include <stdlib.h>
-
 #include <stdint.h>
+#include <stdio.h>
+
 #include <sys/socket.h>
 #include <netpacket/packet.h>
+#include <net/if.h>
 #include <netdb.h>
-#include <ifaddrs.h>
 #include <arpa/inet.h>
 
 #define NI_NUMERICHOST 1
+#define ETH_P_ALL 0x0003
 
-extern int recv_sock;
+#define MAX_PACKET_SIZE 4096
 
-
+int create_recv_socket();
 
 /* void init_device();
 void transform_ip(uint8_t *ip, uint32_t ip_decimal); */
--- src/receive_socket.c
+++ src/receive_socket.c
@@ -1,2 +1,29 @@
 #include "../inc/receive_socket.h"
+#include "../inc/netdevice.h"
 
+int create_recv_socket() {
+    int sock_raw;
+    struct sockaddr_ll sa;
+
+    // Utworzenie gniazda
+    sock_raw = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    if (sock_raw < 0) {
+        fprintf(stderr, "Blad przy tworzeniu gniazda!\n");
+        return -1;
+    }
+
+/* 
+    za pomocą zdefiniowanej na etapie uruchamiania programu zmiennej
+    host_device_ifindex przechowujacej index interfejsu mozemy zbindowac
+    utworzone wczesniej gniazdo do sockaddr_ll
+ */
+    sa.sll_family = AF_PACKET;
+    sa.sll_protocol = htons(ETH_P_ALL);
+    sa.sll_ifindex = host_device_ifindex;
+    if (bind(sock_raw, (struct sockaddr *)&sa, sizeof(sa)) == -1) {
+        fprintf(stderr, "Wystapil blad przy bindowaniu gniazda");
+        return -1;
+    }
+
+    return sock_raw;
+}
